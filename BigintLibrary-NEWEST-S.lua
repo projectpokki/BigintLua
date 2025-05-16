f969375f11e2ac09fc3b6f7d42af3edb836a1e68
projectpokki <158499943+projectpokki@users.noreply.github.com>
2025-05-16 18:18:17 +0800

    Update BigintLibrary-NEWEST-S.lua

--- BigintLibrary-NEWEST-S.lua
+++ BigintLibrary-NEWEST-S.lua
@@ -5,7 +5,7 @@
 |                                                                                |
 +--------------------------------------------------------------------------------+
 |                                                                                |
-| Copyright (c) 2025 Pok Man Chiang                                              |
+| Copyright (c) 2024 Pok Man Chiang                                              |
 |                                                                                |
 | Permission is hereby granted, free of charge, to any person obtaining a copy   |
 | of this software and associated documentation files (the "Software"), to deal  |
